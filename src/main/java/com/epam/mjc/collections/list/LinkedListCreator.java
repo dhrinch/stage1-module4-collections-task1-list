d286481ca3a3e7456898ef567ebbfc7d578f08b2
dhrinch <dhrinch@gmail.com>
2022-10-07 22:14:13 +0200

    Solution

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> resultList = new LinkedList<>();
+        for (Integer element : sourceList) {
+            if(element % 2 == 0) {
+                resultList.addLast(element);
+            } else {
+                resultList.addFirst(element);
+            }
+        }
+        return resultList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int x = Integer.parseInt(a);
+        int y = Integer.parseInt(b);
+        int resultA = 5 * x * x + 3;
+        int resultB = 5 * y * y + 3;
+        int compareResult = Integer.compare(resultA, resultB);
+        return compareResult != 0 ? compareResult : Integer.compare(x, y);
     }
 }
